7e4e1a1a5027938b5853c8c631c30efc0c1d7146
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2023-05-13 19:49:46 +0200

    initial server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{net::TcpListener, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("Server should bind");
+
+    while let Some(stream) = listener.incoming().next() {
+        let mut stream = stream.expect("Stream should be ok");
+        println!("Connection established");
+        println!("{}", stream.peer_addr().expect("Peer should have address"));
+
+        let response = String::from("hello");
+        stream.write(response.as_bytes()).expect("Stream write should succeed");
+    }
+
+    println!("Server exiting");
 }
